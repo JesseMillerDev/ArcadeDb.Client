5f773e059dda6ede3cc6ceb0506cb820ab5ce3fa
JesseMillerDev <jesse.miller9959@gmail.com>
2024-11-24 17:11:02 -0800

    feat: whitespace to kick off build

--- src/ArcadeDB.Client/ArcadeDatabase.cs
+++ src/ArcadeDB.Client/ArcadeDatabase.cs
@@ -3,6 +3,5 @@
 public class ArcadeDatabase : ArcadeDatabaseHandle
 {
     public ArcadeDatabase(ArcadeServer server, string database) : base(server, database) { }
-
     public async Task<ArcadeTransaction> BeginTransaction() => new ArcadeTransaction(this.Server, this.Database, await this.Server.Begin(this.Database));
 }
